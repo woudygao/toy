5f78015f357e552626dfc55ba6eff581405e79ce
gaoweikang <gaoweikang@baidu.com>
2016-03-11 23:33:44 +0800

    [add] 使用回溯的方式重新设计了计算策略
    Signed-off-by: gaoweikang <gaoweikang@baidu.com>

--- solve.php
+++ solve.php
@@ -13,324 +13,93 @@ if ($argc > 1) {
     emptyMap();
 }
 else {
-    solve();
+    $obj = new AdvancedSolve();
+    $obj->init();
+    $obj->beginsolve();
 }
 
-function solve() {
-    $obj = new fileParse();
 
-    $sudoku = $obj->readFromExcel('map.xlsx');
+class AdvancedSolve {
+    //猜测栈
+    private $guessRecord = array();
+    //sudoku栈
+    private $sudokuStack = array();
 
+    const maxdepth = 18;
 
-    $time = 0;
+    private $currentSudoku = null;
+    private $xmlHandle = null;
 
-    try {
-        while ($time < 18) {
-            echo '--------------------------------------------' . PHP_EOL;
-            echo 'Time:' . $time . PHP_EOL;
-            echo 'line check' . PHP_EOL;
+    private $linechecker = null;
+    private $submapchecker = null;
 
-            $before = $sudoku->getDefined();
+    //初始化
+    public function init() {
+        $obj = new fileParse();
+        $sudoku = $obj->readFromExcel('map.xlsx');
+        $this->currentSudoku = $sudoku;
+        $this->xmlHandle = $obj;
+        $this->linechecker = new linechecker();
+        $this->submapchecker = new submapchecker();
+    }
 
-            if (81 == $before) {
-                break;
-            }
+    public function beginsolve() {
 
-            echo 'line check' . PHP_EOL;
-            check($obj, $sudoku, new linechecker(), $time, 'line');
-
-            $after = $sudoku->getDefined();
-            if ($after > $before) {
-                $time ++;
-                continue;
-            }
-
-            echo 'submap check' . PHP_EOL;
-            check($obj, $sudoku, new submapchecker(), $time, 'submap');
-
-            $after = $sudoku->getDefined();
-            if ($after > $before) {
-                $time ++;
-                continue;
-            }
-
-//            echo 'hyper check' . PHP_EOL;
-//            check($obj, $sudoku, new hyperchecker(), $time, 'hyper');
-//
-//            $time ++;
+        $depth = count($this->sudokuStack);
+        if (self::maxdepth < $depth) {
+            //触底了直接false
+            return false;
         }
-    }
-    catch (Exception $e) {
-        echo $e->getMessage() . PHP_EOL;
-    }
 
-    $obj->dumpToXlsx('result.xlsx');
-}
+        $flag = true;
+        $time = 1;
+        while ($flag) {
+            try {
+                $flag = $this->check($depth, $time);
+            }
+            catch (Exception $e) {
+                //如果出现异常，说明一定是guess出错了
+                return false;
+            }
+            $time ++;
+        }
 
-function check(fileParse &$obj, sudokumap &$sudoku, $checker, $time = 0, $name = 'line') {
-    $before = $sudoku->getDefined();
-    $checker->docheck($sudoku);
-    $after = $sudoku->getDefined();
-    if ($after > $before) {
+        $definedcount = $this->currentSudoku->getDefined();
+        if (81 == $definedcount) {
+            //成功了，直接输出结果，然后返回，todo 这里也是可以直接结束运行的
+            $this->xmlHandle->dumpToXlsx('result.xlsx');
+            return true;
+        }
 
-        echo 'All Defined:' . $sudoku->getDefined() . PHP_EOL;
-        $obj->writeToXlsx($sudoku, $time . '-' . $name . '-' . $after);
-    }
-}
+        $guess = $this->currentSudoku->getMinCandiPos();
+        $i = $guess[0];
+        $j = $guess[1];
 
-function emptyMap() {
-    $obj = new fileParse();
-    ob_start();
-    $obj->outputSample();
-    $out = ob_get_contents();
-    ob_end_clean();
-    file_put_contents('map', $out);
-}
+        foreach ($guess[2] as $key => $bol) {
+            $copy = $this->currentSudoku->copySelf();
+            $copy->defineBlock($i, $j, $key);
+            array_push($this->sudokuStack, $this->currentSudoku);
+            $this->currentSudoku = $copy;
 
-
-// class define
-class basicchecker {
-
-    public function docheck(sudokumap &$sudoku, $arrPositions) {
-
-        //1. 遍历找到确定的单元格的值
-        $defind = array();
-
-        $toApplyPosition = array();
-        foreach ($arrPositions as $position) {
-            $i = $position[0];
-            $j = $position[1];
-
-            if (! $sudoku->isBlockMuti($i, $j)) {
-                $block = $sudoku->arrBlocks[$i][$j];
-                $block = array_keys($block);
-                foreach ($block as $v) {
-                    $defind[] = $v;
-                }
+            //完成一次guess 之后再开始下一层
+            $result = $this->beginsolve();
+            if ($result) {
+                return true;
             }
             else {
-                $toApply[0] = $i;
-                $toApply[1] = $j;
-                $toApplyPosition[] = $toApply;
-            }
-        }
-
-        //2. 遍历将不确定单元格中的确定的候选项删除
-        foreach ($toApplyPosition as $position) {
-            $i = $position[0];
-            $j = $position[1];
-
-            if ($sudoku->isBlockMuti($i, $j)) {
-                foreach ($defind as $v) {
-                    $sudoku->removeFromBlock($i, $j, $v);
-                }
-            }
-        }
-
-        $this->checkException($sudoku, $arrPositions);
-
-        $this->docheckDouble($sudoku, $arrPositions, $toApplyPosition);
-
-        $this->checkShowOnce($sudoku, $arrPositions);
-    }
-
-    public function checkShowOnce(sudokumap &$sudoku, $arrPositions) {
-        //检查某个值是否只出现了一次，如果是，则将单元格的值直接更正
-        $defined = array();
-
-        foreach ($arrPositions as $position) {
-            $i = $position[0];
-            $j = $position[1];
-
-            foreach ($sudoku->arrBlocks[$i][$j] as $key => $bol) {
-                $defined[$key][] = $position;
-            }
-        }
-
-        foreach ($defined as $key => $positions) {
-            if (1 == count($positions)) {
-                $i = $positions[0][0];
-                $j = $positions[0][1];
-                $sudoku->defineBlock($i, $j, $key);
-            }
-        }
-
-        $this->checkException($sudoku, $arrPositions);
-    }
-
-    public function docheckDouble(sudokumap &$sudoku, $arrPositions, $toApplyPosition) {
-        //检查二元链, 并应用
-        $candi = array();
-        foreach ($toApplyPosition as $position) {
-            $i = $position[0];
-            $j = $position[1];
-
-            if (2 == $sudoku->countInBlock($i, $j)) {
-                $tmp[0] = $i;
-                $tmp[1] = $j;
-                $candi[] = $tmp;
-            }
-        }
-        //至少有两个以上的才能构成二元关系
-        $twice = array();
-
-        if (2 <= count($candi)) {
-            $size = count($candi);
-            for ($a = 0; $a < $size; $a ++) {
-                if (4 == count($twice)) {
-                    break;
-                }
-                for ($b = $a + 1; $b < $size; $b ++) {
-                    $i1 = $candi[$a][0];
-                    $j1 = $candi[$a][1];
-                    $i2 = $candi[$b][0];
-                    $j2 = $candi[$b][1];
-
-                    if ($sudoku->twoEqual($i1, $j1, $i2, $j2)) {
-                        $twice = array($i1, $j1, $i2, $j2);
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (4 == count($twice)) {
-
-            error_log(json_encode($twice) . PHP_EOL, 3, 'log');
-
-            $values = array_keys($sudoku->arrBlocks[$twice[0]][$twice[1]]);
-
-            foreach ($toApplyPosition as $position) {
-                $ti = $position[0];
-                $tj = $position[1];
-
-                if (($ti == $twice[0] && $tj == $twice[1]) || ($ti == $twice[2] && $tj == $twice[3])) {
-                    continue;
-                }
-                $sudoku->removeFromBlockBatch($ti, $tj, $values);
-            }
-
-        }
-
-        $this->checkException($sudoku, $arrPositions);
-    }
-
-    //检测出现重复值的问题
-    public function checkException(sudokumap &$sudoku, $arrPositions) {
-        $keyDefined = array();
-        foreach ($arrPositions as $position) {
-            $i = $position[0];
-            $j = $position[1];
-            if (! $sudoku->isBlockMuti($i, $j)) {
-                $values = array_keys($sudoku->arrBlocks[$i][$j]);
-                foreach ($values as $v) {
-                    if (! array_key_exists($v, $keyDefined)) {
-                        $keyDefined[$v] = 0;
-                    }
-                    else {
-                        $keyDefined[$v] ++;
-                    }
-                }
-            }
-        }
-
-        foreach ($keyDefined as $k => $v) {
-            if (1 <= $v) {
-                throw new Exception(json_encode($arrPositions));
-            }
-        }
-
-    }
-}
-
-
-class linechecker {
-
-    public function docheck(sudokumap &$sudoku) {
-        $check = new basicchecker();
-
-        //1. 行检查
-        for ($i = 1; $i < 10; $i ++) {
-            $arrPositions = array();
-            for ($j = 1; $j < 10; $j ++) {
-                $position = array();
-                $position[0] = $i;
-                $position[1] = $j;
-                $arrPositions[] = $position;
-            }
-            $check->docheck($sudoku, $arrPositions);
-        }
-
-        //2. 列检查
-        for ($j = 1; $j < 10; $j ++) {
-            $arrPositions = array();
-            for ($i = 1; $i < 10; $i ++) {
-                $position = array();
-                $position[0] = $i;
-                $position[1] = $j;
-                $arrPositions[] = $position;
-            }
-
-            $check->docheck($sudoku, $arrPositions);
-        }
-
-    }
-}
-
-
-class submapchecker {
-
-    public function docheck(sudokumap &$sudoku) {
-
-        $check = new basicchecker();
-        //区块检查
-        $all[] = range(1,3);
-        $all[] = range(4,6);
-        $all[] = range(7,9);
-
-        $allRow = $all;
-        $allColume = $all;
-        foreach ($allRow as $arrRow) {
-            foreach ($allColume as $arrColume) {
-                $arrPositions = $this->getPositons($arrRow, $arrColume);
-                $check->docheck($sudoku, $arrPositions);
+                $this->currentSudoku = array_pop($this->sudokuStack);
+                continue;
             }
         }
 
     }
 
-    public function getPositons($arrRow, $arrColume) {
-        $arrPosition = array();
-        foreach ($arrRow as $i) {
-            foreach ($arrColume as $j) {
-                $position = array();
-                $position[0] = $i;
-                $position[1] = $j;
-                $arrPosition[] = $position;
-            }
-        }
-        return $arrPosition;
+    public function check($depth, $time) {
+        $bolline = $this->linechecker->docheck($this->currentSudoku);
+        $this->xmlHandle->writeToXlsx($this->currentSudoku, $depth . '-' . $time . '-' . 'line' . '-' . $this->currentSudoku->getDefined());
+
+        $bolsubmap = $this->submapchecker->docheck($this->currentSudoku);
+        $this->xmlHandle->writeToXlsx($this->currentSudoku, $depth . '-' . $time . '-' . 'submap' . '-' . $this->currentSudoku->getDefined());
+        return ($bolline || $bolsubmap);
     }
-}
-
-class hyperchecker extends submapchecker {
-
-    public function docheck(sudokumap &$sudoku) {
-
-        $check = new basicchecker();
-        //区块检查
-        $all[] = range(2,4);
-        $all[] = range(6,8);
-
-        $allRow = $all;
-        $allColume = $all;
-        foreach ($allRow as $arrRow) {
-            foreach ($allColume as $arrColume) {
-                $arrPositions = $this->getPositons($arrRow, $arrColume);
-                $check->docheck($sudoku, $arrPositions);
-            }
-        }
-
-    }
-
 }
--- sudokumap.php
+++ sudokumap.php
@@ -11,10 +11,11 @@ require_once 'require.php';
 
 class sudokumap {
 
+    private $isimprove = false; //标志位，是否取得进步，包括进一步削减候选项，或者确定位置的值
     const separator = ", ";
 
     // 9 x 9 map,  行列的序号都是 1~9
-    public $arrBlocks = null;
+    public $arrBlocks = array();
 
     public function addToBlock($i, $j, $v) {
         $this->arrBlocks[$i][$j][$v] = true;
@@ -25,10 +26,14 @@ class sudokumap {
         $this->arrBlocks[$i][$j] = array();
         //然后更正值
         $this->arrBlocks[$i][$j][$v] = true;
+        $this->isimprove = true;
     }
 
     public function removeFromBlock($i, $j, $v){
-        unset($this->arrBlocks[$i][$j][$v]);
+        if (array_key_exists($v, $this->arrBlocks[$i][$j])) {
+            unset($this->arrBlocks[$i][$j][$v]);
+            $this->isimprove = true;
+        }
     }
 
     //用于二元或者多元的消除
@@ -42,7 +47,58 @@ class sudokumap {
         return count($this->arrBlocks[$i][$j]);
     }
 
-    //查看是否是二元链，对称的两个九宫格
+    /**
+     * @desc:重置improve 标志位
+     */
+    public function resetImprove() {
+        $this->isimprove = false;
+    }
+
+    public function isImprove() {
+        return $this->isimprove;
+    }
+
+    /**
+     * @desc:取得候选数最小的位置
+     * @return array （$i, $j, $candis:array()）
+     */
+    public function getMinCandiPos() {
+        $ti = 1;
+        $tj = 1;
+        $min = 9;
+        for ($i = 1; $i < 9; $i ++) {
+            $j = 1;
+            for(; $j < 9; $j ++) {
+                if (1 == count($this->arrBlocks[$i][$j])) {
+                    continue;
+                }
+                else {
+                    if ($min >= count($this->arrBlocks[$i][$j])) {
+                        $min = count($this->arrBlocks[$i][$j]);
+                        $ti = $i;
+                        $tj = $j;
+                    }
+                }
+            }
+        }
+
+        return array($ti, $tj, $this->arrBlocks[$ti][$tj]);
+    }
+
+    public function copySelf() {
+        $newone = new sudokumap();
+        $newone->arrBlocks = $this->arrBlocks;
+        return $newone;
+    }
+
+    /**
+     * @desc: 执行二元检查，如果两个位置是二元位置：即这两个位置的候选数集合是一样的，且都是两个
+     * @param $i1
+     * @param $j1
+     * @param $i2
+     * @param $j2
+     * @return bool
+     */
     public function twoEqual($i1, $j1, $i2, $j2) {
         $value1 = array_keys($this->arrBlocks[$i1][$j1]);
         $value2 = array_keys($this->arrBlocks[$i2][$j2]);
@@ -63,6 +119,11 @@ class sudokumap {
         return false;
     }
 
+    /**
+     * @desc: 初始化满一个位置
+     * @param $i
+     * @param $j
+     */
     public function initFullBlock($i, $j) {
         //值的取值范围为 1~9
         for ($v = 1; $v < 10; $v ++) {
@@ -70,6 +131,10 @@ class sudokumap {
         }
     }
 
+    /**
+     * @desc: 计算确定数字的位置一共有多少个
+     * @return int
+     */
     public function getDefined() {
         $arrRow = range(1, 9);
         $count = 0;
@@ -85,14 +150,29 @@ class sudokumap {
         return $count;
     }
 
+    /**
+     * @desc:判断位置是否为空
+     * @param $i
+     * @param $j
+     * @return bool
+     */
     public function isBlockEmpty($i, $j) {
         return empty($this->arrBlocks[$i][$j]);
     }
 
+    /**
+     * @desc: 判断位置是否有多个候选数
+     * @param $i
+     * @param $j
+     * @return bool
+     */
     public function isBlockMuti($i, $j) {
         return 1 < count($this->arrBlocks[$i][$j]);
     }
 
+    /**
+     * @desc:初始化一个samplemap，用于输出检查
+     */
     public function initSampleMap() {
         for ($i = 1; $i < 10; $i ++) {
             for ($j = 1; $j < 10; $j ++) {
@@ -101,6 +181,9 @@ class sudokumap {
         }
     }
 
+    /**
+     * @desc: 命令行输出，格式不太好看
+     */
     public function output() {
 
         for ($i = 1; $i < 10; $i ++) {
@@ -119,6 +202,10 @@ class sudokumap {
         }
     }
 
+    /**
+     * @desc: 将每个位置格式化为字符串，之后输出
+     * @return mixed
+     */
     public function outputToArray() {
         $arrRow = range(1, 9);
         $arrColume = range(1, 9);
@@ -134,6 +221,10 @@ class sudokumap {
         return $result;
     }
 
+    /**
+     * @desc: 从数组导入，早期调试观察用
+     * @param $arrLine
+     */
     public function inputFromArray($arrLine) {
         $i = 1;
         foreach ($arrLine as $line) {
@@ -163,6 +254,10 @@ class sudokumap {
         }
     }
 
+    /**
+     * @desc:从xlsx解析的结果中导入
+     * @param $values
+     */
     public function inputFromXlsxArr($values) {
         $arrRow = range(1, 9);
         $arrColume = range(1, 9);
